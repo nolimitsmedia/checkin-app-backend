53e16d5fd35f00ed7d8179a071ca51f28ea18b42
Robin <websupport@nolimitsmedia.com>
2025-07-28 14:52:54 +0800

    update users.js gender

--- routes/users.js
+++ routes/users.js
@@ -136,17 +136,18 @@ router.get("/masterlist", authenticate, async (req, res) => {
         u.role,
         u.avatar,
         u.active,
+        u.gender,
         ARRAY_REMOVE(ARRAY_AGG(DISTINCT m.name), NULL) AS ministries,
         ARRAY_REMOVE(ARRAY_AGG(DISTINCT m.id), NULL) AS ministry_ids
       FROM (
-        SELECT id, first_name, last_name, email, role, avatar, COALESCE(active, true) as active FROM users
+        SELECT id, first_name, last_name, email, role, gender, avatar, COALESCE(active, true) as active FROM users
         UNION ALL
-        SELECT id, first_name, last_name, email, role, avatar, COALESCE(active, true) as active FROM elders
+        SELECT id, first_name, last_name, email, role, gender, avatar, COALESCE(active, true) as active FROM elders
       ) u
       LEFT JOIN user_ministries um ON um.user_id = u.id AND u.role != 'elder'
       LEFT JOIN elder_ministries em ON em.elder_id = u.id AND u.role = 'elder'
       LEFT JOIN ministries m ON (m.id = um.ministry_id OR m.id = em.ministry_id)
-      GROUP BY u.id, u.first_name, u.last_name, u.email, u.role, u.avatar, u.active
+      GROUP BY u.id, u.first_name, u.last_name, u.email, u.role, u.avatar, u.active, u.gender
       ORDER BY u.first_name, u.last_name
     `);
 
